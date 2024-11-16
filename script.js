209df96da0e0e08cae4bb65177f880821f33e05b
KsiuTretyakova <hrytsenkoks@gmail.com>
2024-11-16 19:57:17 +0200

    add UTC time

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-function updateClock() {
+function updateClock(time) {
     const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const hours = String(now.getUTCHours() + time).padStart(2, '0');
+    const minutes = String(now.getUTCMinutes()).padStart(2, '0');
+    const seconds = String(now.getUTCSeconds()).padStart(2, '0');
     
     document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+
+    const selectTimeZona = document.getElementById("timeZona");
+    timeZona = parseInt(selectTimeZona.value, 10);
+    console.log(timeZona);
+    
+    selectTimeZona.addEventListener('change', updateClock(timeZona));
+
 }
 
-setInterval(updateClock, 1000);
+let timeZona;
+
+
+// updateClock(timeZona);
+setInterval(updateClock(timeZona), 1000);
+
+
+
+// const difrent_time_UTC = new Date().getTimezoneOffset() / 60;
+// const timeZona = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+// document.getElementById('clock2').textContent = timeZona;
