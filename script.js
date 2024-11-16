3c0d42d079f392395ffdee9e8c275d51fcc0b7fd
KsiuTretyakova <hrytsenkoks@gmail.com>
2024-11-16 19:00:02 +0200

    print clock (local time) on site

--- script.js
+++ script.js
@@ -8,15 +8,3 @@ function updateClock() {
 }
 
 setInterval(updateClock, 1000);
-
-function updateClock_timezona() {
-    const now_local = new Date();
-    const select_timezona = -5; //NewYork
-    const hours = String(now_local.getHours()-2+select_timezona).padStart(2, '0');
-    const minutes = String(now_local.getMinutes()).padStart(2, '0');
-    const seconds = String(now_local.getSeconds()).padStart(2, '0');
-    
-    document.getElementById('timezona').textContent = `${hours}:${minutes}:${seconds}`;
-}
-
-setInterval(updateClock_timezona, 1000);
